c41845bd650be33bde0e56f0a658b5e3e6199f16
glictevo <guillaume.lictevout@gmail.com>
2022-02-25 12:47:22 +0100

    Add button on top right to create lunch

--- frontend/src/LunchList/LunchList.js
+++ frontend/src/LunchList/LunchList.js
@@ -1,14 +1,25 @@
 import './LunchList.css';
+import { useNavigate } from "react-router-dom";
 import LunchCard from "./LunchCard";
+import Button from '@mui/material/Button';
 import SubHeader from "../SubHeader/SubHeader";
 
 const LunchList = ({lunches}) => {
+    let navigate = useNavigate();
+    const onButtonClick = () => {
+        navigate(`/create-lunch`);
+    }
+
     return (
         <>
+            <Button className="createLunchButton" onClick={onButtonClick}>
+                A lunch to propose?
+            </Button>
             <SubHeader
                 title="Hello 👋"
                 subtitle="Who will have the privilege of having lunch with you?"
             />
+
             <div className="lunchesList">
                 {lunches.map(lunch => (
                         <LunchCard
