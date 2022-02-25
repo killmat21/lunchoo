9a6be60081f085d8c7db450bc4753c1dcc47c420
killianmathieu <killian.mathieu@partoo.fr>
2022-02-25 13:51:56 +0100

    join lunch endpoint done

--- backend/lunchoo/app/urls.py
+++ backend/lunchoo/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import User, Lunches
+from .views import User, Lunches, LunchSubscriber
 
 urlpatterns = [
     path('user/', User.as_view()),
     path('lunch/', Lunches.as_view()),
+    path('lunch_subscribe/', LunchSubscriber.as_view()),
 ]
